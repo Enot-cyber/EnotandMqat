65564bb22c5a187870205bfd305c749d4026b549
3not <morgarin19972@gmail.com>
2020-08-01 19:06:07 +0900

    Card

---
+++ Unguard/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unguard
+{
+    class Card
+    {
+        static void Tiw()
+        {
+            Console.WriteLine("This is work!");
+        }
+    }
+}
