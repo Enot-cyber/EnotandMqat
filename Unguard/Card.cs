0c469346d5aa769f6c906e17349945187ef3a75d
kingMQAT1 <kingforclash1@gmail.com>
2020-08-02 11:58:51 +0900

    fix card

--- Unguard/Card.cs
+++ Unguard/Card.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unguard
+namespace Deck
 {
     class Card
     {
-        static void Tiw()
-        {
-            Console.WriteLine("This is work!");
+        private
+            int CardNum;
+            bool CardStatus; //false is card not on deck
+        public
+        Card()
+            {
+            this -> CardNum = 0;
+            this -> CardStatus = true;
         }
+        void SetCardNum( int numerical_arg){}
+        void SetCardStatus(bool boolean_arg ){}
+        int GetCardNum(){}
+        bool GetCardStatus(){}
     }
 }
